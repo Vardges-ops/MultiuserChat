b34181e48e8a8ed3dc0a8cc9039b980d8a5fa3f8
Vardges <76242038+Vardges-ops@users.noreply.github.com>
2023-04-26 23:50:34 +0400

    Forwarded interface init commit (#7)
    Created forwarded message interface interface with base methods
    Added timestamp column to Forwarded message table

---
+++ multiuserchat/core/forwarded_iteractions.py
@@ -0,0 +1,20 @@
+from typing import List, Any, Tuple
+
+from multiuserchat.db_models import engine
+from multiuserchat.db_models.models import ForwardedMessages
+from sqlalchemy.orm import Session
+
+
+class RoomsInterface:  # TODO If the message is deleted, then its row should be deleted from this table
+
+    @staticmethod
+    def get_conv_forwarded_all_msg(conv_id: int) -> List:  # TODO whether this method should return tuple of (forward, origin) msg ids or Session method
+        with Session(bind=engine) as session:
+            conv_data = session.query(ForwardedMessages).filter(ForwardedMessages.conversation_id == conv_id).all()
+        return [(obj.origin_id, obj.forward_id, obj.timestamp) for obj in conv_data]
+
+    @staticmethod
+    def from_forward_get_origin_with_conv(forward_id: int) -> Tuple[Any, Any]:
+        with Session(bind=engine) as session:
+            forward_msg = session.query(ForwardedMessages).filter(ForwardedMessages.forward_id == forward_id).one_or_none()
+        return forward_msg.origin_id, forward_msg.conversation_id
